5ce1b3267eafdf2c6b03b131952b794133c34a7f
ChrisPTheCoder <ppham13@my.centennialcollege.ca>
2019-01-31 16:39:32 -0500

    first commit

---
+++ PhuongLinhPham_COMP254Lab1_Ex1/src/singlyLinkedList/SinglyLinkedList.java
@@ -0,0 +1,307 @@
+package singlyLinkedList;
+
+import java.util.Scanner;
+
+/**
+ * A basic singly linked list implementation.
+ *
+ * @author Michael T. Goodrich
+ * @author Roberto Tamassia
+ * @author Michael H. Goldwasser
+ */
+public class SinglyLinkedList<E> implements Cloneable {
+  //---------------- nested Node class ----------------
+  /**
+   * Node of a singly linked list, which stores a reference to its
+   * element and to the subsequent node in the list (or null if this
+   * is the last node).
+   */
+  private static class Node<E> {
+
+    /** The element stored at this node */
+    private E element;            // reference to the element stored at this node
+
+    /** A reference to the subsequent node in the list */
+    private Node<E> next;         // reference to the subsequent node in the list
+
+    /**
+     * Creates a node with the given element and next node.
+     *
+     * @param e  the element to be stored
+     * @param n  reference to a node that should follow the new node
+     */
+    public Node(E e, Node<E> n) {
+      element = e;
+      next = n;
+    }
+
+    // Accessor methods
+    /**
+     * Returns the element stored at the node.
+     * @return the element stored at the node
+     */
+    public E getElement() { return element; }
+
+    /**
+     * Returns the node that follows this one (or null if no such node).
+     * @return the following node
+     */
+    public Node<E> getNext() { return next; }
+
+    // Modifier methods
+    /**
+     * Sets the node's next reference to point to Node n.
+     * @param n    the node that should follow this one
+     */
+    public void setNext(Node<E> n) { next = n; }
+    
+  } //----------- end of nested Node class -----------
+
+  // instance variables of the SinglyLinkedList
+  /** The head node of the list */
+  private Node<E> head = null;               // head node of the list (or null if empty)
+
+  /** The last node of the list */
+  private Node<E> tail = null;               // last node of the list (or null if empty)
+
+  /** Number of nodes in the list */
+  private int size = 0;                      // number of nodes in the list
+
+  /** Constructs an initially empty list. */
+  public SinglyLinkedList() { }              // constructs an initially empty list
+
+  // access methods
+  /**
+   * Returns the number of elements in the linked list.
+   * @return number of elements in the linked list
+   */
+  public int size() { return size; }
+
+  /**
+   * Tests whether the linked list is empty.
+   * @return true if the linked list is empty, false otherwise
+   */
+  public boolean isEmpty() { return size == 0; }
+
+  /**
+   * Returns (but does not remove) the first element of the list
+   * @return element at the front of the list (or null if empty)
+   */
+  public E first() {             // returns (but does not remove) the first element
+    if (isEmpty()) return null;
+    return head.getElement();
+  }
+
+  /**
+   * Returns (but does not remove) the last element of the list.
+   * @return element at the end of the list (or null if empty)
+   */
+  public E last() {              // returns (but does not remove) the last element
+    if (isEmpty()) return null;
+    return tail.getElement();
+  }
+
+  // update methods
+  /**
+   * Adds an element to the front of the list.
+   * @param e  the new element to add
+   */
+  public void addFirst(E e) {                // adds element e to the front of the list
+    head = new Node<>(e, head);              // create and link a new node
+    if (size == 0)
+      tail = head;                           // special case: new node becomes tail also
+    size++;
+  }
+
+  /**
+   * Adds an element to the end of the list.
+   * @param e  the new element to add
+   */
+  public void addLast(E e) {                 // adds element e to the end of the list
+    Node<E> newest = new Node<>(e, null);    // node will eventually be the tail
+    if (isEmpty())
+      head = newest;                         // special case: previously empty list
+    else
+      tail.setNext(newest);                  // new node after existing tail
+    tail = newest;                           // new node becomes the tail
+    size++;
+  }
+
+  /**
+   * Removes and returns the first element of the list.
+   * @return the removed element (or null if empty)
+   */
+  public E removeFirst() {                   // removes and returns the first element
+    if (isEmpty()) return null;              // nothing to remove
+    E answer = head.getElement();
+    head = head.getNext();                   // will become null if list had only one node
+    size--;
+    if (size == 0)
+      tail = null;                           // special case as list is now empty
+    return answer;
+  }
+
+  @SuppressWarnings({"unchecked"})
+  public boolean equals(Object o) {
+    if (o == null) return false;
+    if (getClass() != o.getClass()) return false;
+    SinglyLinkedList other = (SinglyLinkedList) o;   // use nonparameterized type
+    if (size != other.size) return false;
+    Node walkA = head;                               // traverse the primary list
+    Node walkB = other.head;                         // traverse the secondary list
+    while (walkA != null) {
+      if (!walkA.getElement().equals(walkB.getElement())) return false; //mismatch
+      walkA = walkA.getNext();
+      walkB = walkB.getNext();
+    }
+    return true;   // if we reach this, everything matched successfully
+  }
+
+  @SuppressWarnings({"unchecked"})
+  public SinglyLinkedList<E> clone() throws CloneNotSupportedException {
+    // always use inherited Object.clone() to create the initial copy
+    SinglyLinkedList<E> other = (SinglyLinkedList<E>) super.clone(); // safe cast
+    if (size > 0) {                    // we need independent chain of nodes
+      other.head = new Node<>(head.getElement(), null);
+      Node<E> walk = head.getNext();      // walk through remainder of original list
+      Node<E> otherTail = other.head;     // remember most recently created node
+      while (walk != null) {              // make a new node storing same element
+        Node<E> newest = new Node<>(walk.getElement(), null);
+        otherTail.setNext(newest);     // link previous node to this one
+        otherTail = newest;
+        walk = walk.getNext();
+      }
+    }
+    return other;
+  }
+
+  public int hashCode() {
+    int h = 0;
+    for (Node walk=head; walk != null; walk = walk.getNext()) {
+      h ^= walk.getElement().hashCode();      // bitwise exclusive-or with element's code
+      h = (h << 5) | (h >>> 27);              // 5-bit cyclic shift of composite code
+    }
+    return h;
+  }
+
+  /**
+   * Produces a string representation of the contents of the list.
+   * This exists for debugging purposes only.
+   */
+  public String toString() {
+    StringBuilder sb = new StringBuilder("(");
+    Node<E> walk = head;
+    while (walk != null) {
+      sb.append(walk.getElement());
+      if (walk != tail)
+        sb.append(", ");
+      walk = walk.getNext();
+    }
+    sb.append(")");
+    return sb.toString();
+  }
+  //main method
+  public static void main(String[] args)
+  {
+	  
+	  SinglyLinkedList<String> list = new SinglyLinkedList<String>();
+	  list.addFirst("MSP");
+	  list.addLast("ATL");
+	  list.addLast("BOS");
+	  //
+	  list.addFirst("LAX");
+	  System.out.println("Original List: " + list);
+	  
+	//SWAP NODES
+	  @SuppressWarnings("resource")
+	  Scanner in = new Scanner(System.in); 
+	  System.out.println("Enter first element to be swapped:");
+	  
+	   // Reading data using readLine 
+	   String swapNode1 = in.nextLine(); 
+	   
+	   System.out.println("Enter second element to be swapped:");
+	  
+	   String swapNode2 = in.nextLine();  
+	
+	  list.swapNodes(swapNode1,swapNode2);
+	  
+	  System.out.println("After Swapping "+ swapNode1 + " with " + swapNode2 + ": " + list);
+  }
+  
+  public void swapNodes(String node1Val, String node2Val) {
+	  
+	  /*Cases to be handled
+	  1. Both node1Val and node2Val can be same
+	  2. Either node or node 2 may not be present
+	  2. Either node1Val or node2Val can be of a head node
+	  3. Either node1Val or node2Val can be of tail node
+	  4. node1Val and node2Val may not be adjacent	  
+	  */
+	  
+	  
+	  //CASE1: node1 and node2 val is same
+	  if(node1Val.equals(node2Val)) {
+		  //do nothing
+		  System.out.println("ERROR: Both values are same!");
+		  return;
+	  }
+	  Node<E> prevNode1 = null;
+	  Node<E> currNode1 = head;
+	  
+	  Node<E> prevNode2 = null;
+	  Node<E> currNode2 = head;
+	
+	  //Traverse and track nodes and their previous nodes
+	  
+	  //Traverse the list to find node1Val
+	  while(currNode1 != null && !currNode1.getElement().equals(node1Val)) {
+		  prevNode1 = currNode1;
+		  currNode1 = currNode1.getNext();
+	  }
+	  
+	  //Traverse the list to find node2Val	  
+	  while(currNode2 != null && !currNode2.getElement().equals(node2Val) ) {
+		  prevNode2 = currNode2;
+		  currNode2 = currNode2.getNext();
+	  }
+	  
+	  
+	  //CASE2: Check if either node 1 or node 2 is not present
+	  if(currNode1 == null || currNode2 == null) {
+		  System.out.println("ERROR: One of the value is not present in the list!");
+		  return;
+	  }
+	  
+	  
+	  //CASE3: either node 1 and node2 can be head node
+	  if(prevNode1 == null) {//The node1Val is at the head of the list
+		  head = currNode2; //Make node2 as head of list
+	  }else { //node1Val is not at the head of the list
+		  prevNode1.setNext(currNode2); //Link Next pointer of prevNode1 to node2
+	  }
+	  
+	  if(prevNode2 == null) {//The node2Val is at the head of the list
+		  head = currNode1; //Make node1 as head of list
+	  }else { //node2Val is not at the head of the list
+		  prevNode2.setNext(currNode1); //Link Next pointer of prevNode2 to node1
+	  }
+	  
+	
+	  //CASE4: Either node 1 and node 2 can be tail node
+
+	  if(currNode2.getNext() == null) { //Node2 is last element
+		//  currNode1.setNext(null);
+		  tail = currNode1;
+	  }else if(currNode1.getNext() == null) { //Node1 is last element	
+		  //currNode2.setNext(null);
+		  tail = currNode2;
+	  }	  
+	
+	  
+	  //CASE 5: Everything good, Do swapping
+	  Node<E> temp = currNode1.getNext(); //Create temporary node to save node1's next node		 
+	  currNode1.setNext(currNode2.getNext());  
+	  currNode2.setNext(temp);
+  }
+}
